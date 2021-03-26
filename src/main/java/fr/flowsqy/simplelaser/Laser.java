943ed1e636e7be5af699600aadea2d952ad2846d
Flowsqy <Flo504.Dev@gmail.com>
2021-03-26 21:58:34 +0100

    Add Process class (from SkytAsul cf. https://github.com/SkytAsul/GuardianBeam)

--- src/main/java/fr/flowsqy/simplelaser/Laser.java
+++ src/main/java/fr/flowsqy/simplelaser/Laser.java
@@ -1,11 +1,17 @@
 package fr.flowsqy.simplelaser;
 
 import fr.flo504.reflect.Reflect;
+import org.apache.commons.lang.Validate;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -181,7 +187,7 @@ public class Laser {
         Reflect.set(zdField, packet, 0);
         Reflect.set(yRotField, packet, (byte) (location.getYaw() * 256.0F / 360.0F));
         Reflect.set(xRotField, packet, (byte) (location.getPitch() * 256.0F / 360.0F));
-        Reflect.set(yHeadRotField, packet, 0);
+        Reflect.set(yHeadRotField, packet, (byte)0);
 
         return packet;
     }
@@ -198,5 +204,122 @@ public class Laser {
         Reflect.invoke(registerDataWatcherMethod, dataWatcher, dataIdAttackTarget, squidId);
         return Reflect.newInstance(packetMetaDataConstructor, id, dataWatcher, true);
     }
-    
+
+    private static Object getDestroyEntityPacket(int squidId, int guardianId){
+        return Reflect.newInstance(packetEntityDestroyConstructor, (Object) new int[]{squidId, guardianId});
+    }
+
+    private static Object getPlayerConnection(Player player){
+        return Reflect.get(playerConnectionField, Reflect.invoke(getHandleMethod, player));
+    }
+
+    private static void sendPacket(Object playerConnection, Object packet){
+        Reflect.invoke(sendPacketMethod, playerConnection, packet);
+    }
+
+    private final int duration;
+    private final int distanceSquared;
+    private final Location start;
+    private final Location end;
+
+    private final Object createSquidPacket;
+    private final Object createGuardianPacket;
+    private final Object metaDataSquidPacket;
+    private final Object metaDataGuardianPacket;
+    private final Object destroyPacket;
+
+    private BukkitRunnable runnable;
+
+    /*
+     * Inspired by the API <a href="https://www.spigotmc.org/resources/guardianbeamapi.18329">GuardianBeamAPI</a></br>
+     * @see <a href="https://github.com/SkytAsul/GuardianBeam">GitHub page</a>
+     * @author SkytAsul (for the task management)
+     */
+
+    /**
+     * Create a Laser instance
+     * @param start Location where laser will starts
+     * @param end Location where laser will ends
+     * @param duration Duration of laser in seconds (<i>-1 if infinite</i>)
+     * @param distance Distance where laser will be visible
+     */
+    public Laser(Location start, Location end, int duration, int distance) {
+        this.start = start;
+        this.end = end;
+        this.duration = duration;
+        distanceSquared = distance * distance;
+
+        final int squid = entityCount.incrementAndGet();
+        final int guardian = entityCount.incrementAndGet();
+        createSquidPacket = getSpawnPacket(squid, end, SQUID_ID);
+        createGuardianPacket = getSpawnPacket(guardian, start, GUARDIAN_ID);
+        metaDataSquidPacket = getSquidMetaDataPacket(squid);
+        metaDataGuardianPacket = getGuardianMetaDataPacket(guardian, squid);
+
+        destroyPacket = getDestroyEntityPacket(squid, guardian);
+    }
+
+    public void start(Plugin plugin){
+        Validate.isTrue(runnable == null, "Task already started");
+        runnable = new BukkitRunnable() {
+            int time = duration;
+            final Set<Player> show = new HashSet<>();
+            @Override
+            public void run() {
+                try {
+                    if (time == 0){
+                        cancel();
+                        return;
+                    }
+                    for (Player p : start.getWorld().getPlayers()){
+                        if (isCloseEnough(p.getLocation())){
+                            if (!show.contains(p)){
+                                sendStartPackets(p);
+                                show.add(p);
+                            }
+                        }else if (show.contains(p)){
+                            sendPacket(getPlayerConnection(p), destroyPacket);
+                            show.remove(p);
+                        }
+                    }
+                    if (time != -1) time--;
+                }catch (ReflectiveOperationException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public synchronized void cancel() throws IllegalStateException {
+                super.cancel();
+                for (Player p : show){
+                    sendPacket(getPlayerConnection(p), destroyPacket);
+                }
+                runnable = null;
+            }
+        };
+        runnable.runTaskTimer(plugin, 0L, 20L);
+    }
+
+    public void stop(){
+        Validate.isTrue(runnable != null, "Task not started");
+        runnable.cancel();
+    }
+
+    public boolean isStarted(){
+        return runnable != null;
+    }
+
+    private void sendStartPackets(Player p) throws ReflectiveOperationException{
+        final Object playerConnection = getPlayerConnection(p);
+        sendPacket(playerConnection, createSquidPacket);
+        sendPacket(playerConnection, metaDataSquidPacket);
+        sendPacket(playerConnection, createGuardianPacket);
+        sendPacket(playerConnection, metaDataGuardianPacket);
+    }
+
+    private boolean isCloseEnough(Location location) {
+        return start.distanceSquared(location) <= distanceSquared ||
+                end.distanceSquared(location) <= distanceSquared;
+    }
+
 }
